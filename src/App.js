e2fdb153535946a99f5f59f0668676aec2474400
Iliya Lunev <lunevig90@gmail.com>
2021-10-25 20:02:56 +0300

    add search

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import AppBar from './componets/AppBar/AppBar';
 import { Switch, Route } from 'react-router-dom';
 import HomePageView from './componets/Pages/View/HomePageView';
 import MovieDetailsPageViews from './componets/Pages/View/MovieDetailsPageViews';
-import MoviesPage from './componets/Pages/MoviesPage/MoviesPage';
+import MoviesPageView from './componets/Pages/View/MoviesPageView';
 import NotFoundViews from './componets/Pages/View/NotFoundViews';
 
 export default function App() {
@@ -16,7 +16,7 @@ export default function App() {
         </Route>
 
         <Route path="/movies">
-          <MoviesPage />
+          <MoviesPageView />
         </Route>
 
         <Route path="/movies/:movieId">
--- src/componets/Pages/MoviesPage/MoviesPage.js
+++ src/componets/Pages/MoviesPage/MoviesPage.js
@@ -1,3 +1,79 @@
+import { useState, useEffect } from 'react';
+import * as ApiService from '../../../Service/ApiService';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import 'react-toastify/dist/ReactToastify.css';
+import s from './moviesPage.module.css';
+
 export default function MoviesPage() {
-  return <h2>Фильмы</h2>;
+  const [query, setQuery] = useState('');
+  const history = useHistory();
+  const location = useLocation();
+  const [inputQuery, setInputQuery] = useState('');
+  const [movies, setMovies] = useState(null);
+
+  if (location.search !== '') {
+    if (query === '') {
+      setQuery(new URLSearchParams(location.search).get('query'));
+    }
+  }
+
+  useEffect(() => {
+    if (query === '') {
+      return;
+    }
+    ApiService.fetchMovies({ query }).then(data => setMovies(data.results));
+    history.push({ ...location, search: `query=${query}` });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
+
+  const handleInputChange = e => {
+    setInputQuery(e.currentTarget.value.toLowerCase());
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (inputQuery.trim() === '') {
+      return;
+    }
+    setQuery(inputQuery);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className={s.form}>
+        <input
+          className={s.input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Enter the title of the movie"
+          value={inputQuery}
+          onChange={handleInputChange}
+        ></input>
+        <button type="submit" className={s.button}>
+          Search
+        </button>
+      </form>
+      <ul>
+        {movies &&
+          movies.map(movie => {
+            return (
+              <li key={movie.id}>
+                <Link
+                  to={{
+                    pathname: `/movies/${movie.id}`,
+                    state: { from: location },
+                  }}
+                >
+                  {movie.title}
+                </Link>
+              </li>
+            );
+          })}
+        {movies && movies.length === 0 && (
+          <p className={s.title}>No results were found for {query}</p>
+        )}
+      </ul>
+    </>
+  );
 }
