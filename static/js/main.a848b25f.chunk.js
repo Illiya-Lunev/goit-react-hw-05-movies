bc3c0bc7e2952888711892ec259148559fcf3243
Iliya Lunev <lunevig90@gmail.com>
2021-10-25 10:20:17 +0300

    Updates

---
+++ static/js/main.a848b25f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-05-movies"]=this["webpackJsonpgoit-react-hw-05-movies"]||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c.n(n),s=c(3),i=c.n(s),o=c(0);var a=function(){return Object(o.jsx)("div",{className:"App",children:Object(o.jsx)("h1",{children:"React"})})};i.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(a,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.a848b25f.chunk.js.map
